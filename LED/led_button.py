320d3791cb2cf9ed357575d7f7874bcc9637a0a0
Bert Carremans <bert.carremans@gmail.com>
2018-12-02 14:21:54 +0100

    led button script

---
+++ LED/led_button.py
@@ -0,0 +1,15 @@
+from gpiozero import LED, Button
+from signal import pause
+
+
+led = LED(25)
+button = Button(2)
+
+button.when_pressed = led.on
+button.when_released = led.off
+
+
+pause()
+
+led.close()
+button.close()
