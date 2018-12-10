74346fa50ca15e24c668df195d1f4e6b090ae279
Bert Carremans <bert.carremans@gmail.com>
2018-12-10 13:43:04 +0100

    camera scripts

---
+++ CAMERA/record_file.py
@@ -0,0 +1,10 @@
+import picamera
+
+camera = picamera.PiCamera()
+
+camera.resolution = (640,480)
+camera.start_recording('videotest.h264')
+camera.wait_recording(10)
+camera.stop_recording()
+
+print('finished recording')
